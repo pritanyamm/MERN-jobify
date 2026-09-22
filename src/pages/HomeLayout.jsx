3420669c83d8867d8b5f496a0441713a5c7b642e
pritanya mm <pritanyamm@gmail.com>
2026-09-22 20:25:09 +0530

    landing page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
   return (
-    <div>
-      <h1>navbar</h1>
+    <>
+      
       <Outlet />
-    </div>
+    </>
   );
 };
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import styled from "styled-components"
 
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import logo from '../assets/images/logo.svg';
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing page</h1>
-      <div className='content'>some content</div>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+
+          <p>
+            Blue umbrellas march sideways past the old clock tower.
+            A silent radio hums tunes from nineteen-eighty-four.
+            Coffee steam writes cursive questions against the pane.
+          </p>
+
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+
+          <Link to="/login" className="btn login-link">
+            Login / Demo User
+          </Link>
+        </div>
+
+        <img
+          src={main}
+          alt="job hunt"
+          className="img main-img"
+        />
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
-  background: red;
-  h1 {
-    color: white;
-  }
-  .content {
-    background: blue; 
-    color: yellow;
-  }
-`;
-export default Landing 
+export default Landing;
+
