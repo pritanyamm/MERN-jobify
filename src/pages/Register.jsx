e58bbe40eeb44988f4c5a3af4880677325490678
pritanya mm <pritanyamm@gmail.com>
2026-09-24 00:23:51 +0530

    register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow } from '../components';
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div> 
-  )
-}
+    <Wrapper>
+      <form className="form">
+       
+        <h4>Register</h4>
+        <FormRow type= 'text' name='name' defaultValue='pritanya'/>
+        <FormRow 
+          type= 'text'
+          name='lastName'
+          labelText='last name'
+          defaultValue='muler'
+        />
+        <FormRow type= 'text' name='location'
+         defaultValue='earth'/>
+        <FormRow type= 'email' name='email' 
+        defaultValue='pritanya@gmail.com'/>
+        <FormRow type= 'password' name='password' 
+        defaultValue='secret123'/>
+
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
 
 export default Register;
